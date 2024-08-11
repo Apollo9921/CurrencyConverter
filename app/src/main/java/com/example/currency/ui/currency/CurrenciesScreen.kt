736c8a410df64351c3087e1b2fef6286a07ab858
Nuno Silva <nunosilvaw33d@gmail.com>
2024-08-11 11:14:54 +0100

    implement search by currency

--- app/src/main/java/com/example/currency/ui/currency/CurrenciesScreen.kt
+++ app/src/main/java/com/example/currency/ui/currency/CurrenciesScreen.kt
@@ -15,17 +15,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.rememberLazyStaggeredGridState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
@@ -35,13 +39,18 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.currency.R
 import com.example.currency.core.BackgroundColor
 import com.example.currency.core.Black
 import com.example.currency.core.TextColor
@@ -68,6 +77,8 @@ private var rateValue = derivedStateOf { rate }
 private var allCurrencyRates: Currency? = null
 private var openDialog = mutableStateOf(false)
 private var isConnected by mutableStateOf(false)
+private var searchByCurrency = mutableStateOf("")
+private var searchByCurrencyValue = derivedStateOf { searchByCurrency }
 
 @Composable
 fun CurrenciesScreen(navController: NavHostController) {
@@ -151,30 +162,75 @@ private fun GetCurrencyRates(navController: NavHostController) {
 
 @Composable
 private fun CurrenciesScreenTopBar() {
-    Row(
+    Column(
         modifier = Modifier
             .fillMaxWidth()
             .background(TopBarColor)
-            .padding(top = 60.dp, start = 20.dp, end = 20.dp, bottom = 20.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween
+            .padding(top = 60.dp, start = 20.dp, end = 20.dp, bottom = 20.dp)
     ) {
-        Text(
-            text = "${rateValue.value.value?.base ?: "EUR"} ${rateValue.value.value?.amount ?: "0.0"}",
-            color = White,
-            fontWeight = FontWeight.W400,
-            fontSize =
-            if (mediaQueryWidth() <= small) {
-                20.sp
-            } else if (mediaQueryWidth() <= normal) {
-                24.sp
-            } else {
-                28.sp
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "${rateValue.value.value?.base ?: "EUR"} ${rateValue.value.value?.amount ?: "0.0"}",
+                color = White,
+                fontWeight = FontWeight.W400,
+                fontSize =
+                if (mediaQueryWidth() <= small) {
+                    20.sp
+                } else if (mediaQueryWidth() <= normal) {
+                    24.sp
+                } else {
+                    28.sp
+                }
+            )
+            Button(onClick = { openDialog.value = true }) {
+                Text(stringResource(id = R.string.change_currency))
             }
-        )
-        Button(onClick = { openDialog.value = true }) {
-            Text("Change Currency")
         }
+        Spacer(modifier = Modifier.padding(5.dp))
+        TextField(
+            value = searchByCurrency.value,
+            onValueChange = {
+                if (it.length <= 10) {
+                    searchByCurrency.value = it
+                }
+            },
+            placeholder = {
+                Text(
+                    text = stringResource(id = R.string.search_by_currency),
+                    color = White
+                )
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+            maxLines = 1,
+            colors = TextFieldDefaults.colors(
+                focusedTextColor = White,
+                unfocusedTextColor = White,
+                focusedIndicatorColor = White,
+                focusedContainerColor = Color.Transparent,
+                unfocusedContainerColor = Color.Transparent,
+                cursorColor = White
+            ),
+            textStyle = TextStyle(
+                fontSize =
+                if (mediaQueryWidth() <= small) {
+                    15.sp
+                } else if (mediaQueryWidth() <= normal) {
+                    20.sp
+                } else {
+                    25.sp
+                },
+                fontWeight = FontWeight.W500,
+                textAlign = TextAlign.Start,
+                color = White
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+        )
     }
 }
 
@@ -184,14 +240,19 @@ private fun ChangeCurrency() {
     var selectedOption by remember { mutableStateOf("") }
     AlertDialog(
         onDismissRequest = { openDialog.value = false },
-        title = { Text("Choose an option") },
+        title = { Text(stringResource(id = R.string.select_option)) },
         text = {
             LazyColumn {
                 items(allCurrencyRates?.currencies?.size ?: 0) { index ->
                     Row(verticalAlignment = Alignment.CenterVertically) {
                         RadioButton(
-                            selected = selectedOption == allCurrencyRates?.currencies?.keys?.elementAt(index),
-                            onClick = { selectedOption = allCurrencyRates?.currencies?.keys?.elementAt(index) ?: "" }
+                            selected = selectedOption == allCurrencyRates?.currencies?.keys?.elementAt(
+                                index
+                            ),
+                            onClick = {
+                                selectedOption =
+                                    allCurrencyRates?.currencies?.keys?.elementAt(index) ?: ""
+                            }
                         )
                         Text(allCurrencyRates?.currencies?.keys?.elementAt(index) ?: "")
                     }
@@ -199,27 +260,32 @@ private fun ChangeCurrency() {
             }
         },
         confirmButton = {
+            val selectOption = stringResource(id = R.string.select_option)
+            val noInternetConnection = stringResource(id = R.string.no_internet)
+            val ok = stringResource(id = R.string.ok)
             Button(onClick = {
                 if (selectedOption.isEmpty()) {
-                    Toast.makeText(context, "Please select an option", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(context, selectOption, Toast.LENGTH_SHORT).show()
                     return@Button
                 }
                 if (status == ConnectivityObserver.Status.Unavailable) {
-                    Toast.makeText(context, "No internet connection", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(context, noInternetConnection, Toast.LENGTH_SHORT).show()
                     return@Button
                 }
+                searchByCurrency.value = ""
                 openDialog.value = false
                 viewModel.isLoading.value = true
                 viewModel.isSuccess.value = false
                 allCurrencyRates?.currencies?.remove(selectedOption)
                 viewModel.getAllCurrencies(status, selectedOption)
             }) {
-                Text("OK")
+                Text(ok)
             }
         },
         dismissButton = {
+            val cancel = stringResource(id = R.string.cancel)
             Button(onClick = { openDialog.value = false }) {
-                Text("Cancel")
+                Text(cancel)
             }
         },
         modifier = Modifier
@@ -233,6 +299,17 @@ private fun RatesList(navController: NavHostController) {
     if (openDialog.value) {
         ChangeCurrency()
     }
+    var sortedCurrencies: Map<String, String>? = null
+    if (searchByCurrencyValue.value.value.isNotEmpty()) {
+        val filteredCurrencies =
+            allCurrencyRates?.currencies?.filterKeys {
+                it.contains(
+                    searchByCurrencyValue.value.value,
+                    ignoreCase = true
+                )
+            }
+        sortedCurrencies = filteredCurrencies?.toSortedMap()
+    }
     val state = rememberLazyStaggeredGridState()
     LazyVerticalStaggeredGrid(
         columns = StaggeredGridCells.Fixed(2),
@@ -244,65 +321,150 @@ private fun RatesList(navController: NavHostController) {
             .background(BackgroundColor)
             .padding(25.dp)
     ) {
-        items(allCurrencyRates?.currencies?.size ?: 0) { index ->
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(
-                        color = White,
-                        shape = RoundedCornerShape(13.dp)
-                    )
-                    .border(
-                        border = BorderStroke(3.dp, Black),
-                        shape = RoundedCornerShape(13.dp)
-                    )
-                    .padding(20.dp)
-                    .clickable {
-                        navController.navigate(Destination.E2(
-                            from = rateValue.value.value?.base ?: "EUR",
-                            to = allCurrencyRates?.currencies?.keys?.elementAt(index) ?: "",
-                            amount = rateValue.value.value?.amount.toString()
-                        ))
-                    },
-                contentAlignment = Alignment.Center
-            ) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    Text(
-                        text = allCurrencyRates?.currencies?.keys?.elementAt(index) ?: "",
-                        color = TextColor,
-                        fontSize =
-                        if (mediaQueryWidth() <= small) {
-                            20.sp
-                        } else if (mediaQueryWidth() <= normal) {
-                            24.sp
-                        } else {
-                            28.sp
-                        },
-                        textAlign = TextAlign.Center,
-                        fontWeight = FontWeight.W500
-                    )
-                    Spacer(modifier = Modifier.padding(10.dp))
-                    val rate = allCurrencyRates?.currencies?.values?.elementAt(index).toString().toDouble()
-                    val rateValue = DecimalFormat("#.##").format(rate)
-                    Text(
-                        text = rateValue,
-                        color = TextColor,
-                        fontSize =
-                        if (mediaQueryWidth() <= small) {
-                            20.sp
-                        } else if (mediaQueryWidth() <= normal) {
-                            24.sp
-                        } else {
-                            28.sp
-                        },
-                        textAlign = TextAlign.Center,
-                        fontWeight = FontWeight.Bold
-                    )
-                }
+        if (sortedCurrencies != null) {
+            items(sortedCurrencies.size) {
+                FilterCurrencyItem(it, sortedCurrencies, navController)
             }
+        } else {
+            items(allCurrencyRates?.currencies?.size ?: 0) { index ->
+                CurrencyItem(index, navController)
+            }
+        }
+    }
+}
+
+@Composable
+private fun FilterCurrencyItem(
+    index: Int,
+    sortedCurrencies: Map<String, String>,
+    navController: NavHostController
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                color = White,
+                shape = RoundedCornerShape(13.dp)
+            )
+            .border(
+                border = BorderStroke(3.dp, Black),
+                shape = RoundedCornerShape(13.dp)
+            )
+            .padding(20.dp)
+            .clickable {
+                searchByCurrency.value = ""
+                navController.navigate(
+                    Destination.E2(
+                        from = rateValue.value.value?.base ?: "EUR",
+                        to = sortedCurrencies.keys.elementAt(index),
+                        amount = rateValue.value.value?.amount.toString()
+                    )
+                )
+            },
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = sortedCurrencies.keys.elementAt(index),
+                color = TextColor,
+                fontSize =
+                if (mediaQueryWidth() <= small) {
+                    20.sp
+                } else if (mediaQueryWidth() <= normal) {
+                    24.sp
+                } else {
+                    28.sp
+                },
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.W500
+            )
+            Spacer(modifier = Modifier.padding(10.dp))
+            val rate = sortedCurrencies.values.elementAt(index).toString().toDouble()
+            val rateValue = DecimalFormat("#.##").format(rate)
+            Text(
+                text = rateValue,
+                color = TextColor,
+                fontSize =
+                if (mediaQueryWidth() <= small) {
+                    20.sp
+                } else if (mediaQueryWidth() <= normal) {
+                    24.sp
+                } else {
+                    28.sp
+                },
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+}
+
+@Composable
+private fun CurrencyItem(index: Int, navController: NavHostController) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                color = White,
+                shape = RoundedCornerShape(13.dp)
+            )
+            .border(
+                border = BorderStroke(3.dp, Black),
+                shape = RoundedCornerShape(13.dp)
+            )
+            .padding(20.dp)
+            .clickable {
+                searchByCurrency.value = ""
+                navController.navigate(
+                    Destination.E2(
+                        from = rateValue.value.value?.base ?: "EUR",
+                        to = allCurrencyRates?.currencies?.keys?.elementAt(index) ?: "",
+                        amount = rateValue.value.value?.amount.toString()
+                    )
+                )
+            },
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = allCurrencyRates?.currencies?.keys?.elementAt(index) ?: "",
+                color = TextColor,
+                fontSize =
+                if (mediaQueryWidth() <= small) {
+                    20.sp
+                } else if (mediaQueryWidth() <= normal) {
+                    24.sp
+                } else {
+                    28.sp
+                },
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.W500
+            )
+            Spacer(modifier = Modifier.padding(10.dp))
+            val rate =
+                allCurrencyRates?.currencies?.values?.elementAt(index).toString()
+                    .toDouble()
+            val rateValue = DecimalFormat("#.##").format(rate)
+            Text(
+                text = rateValue,
+                color = TextColor,
+                fontSize =
+                if (mediaQueryWidth() <= small) {
+                    20.sp
+                } else if (mediaQueryWidth() <= normal) {
+                    24.sp
+                } else {
+                    28.sp
+                },
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold
+            )
         }
     }
 }
